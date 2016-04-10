f69767d76df8502add95f02681e46980d9b6838b
shubh24 <shubhankar24@gmail.com>
2016-04-10 21:42:14 +0530

    Many strategies tried - historical and recent separated

--- passenger.py
+++ passenger.py
@@ -1,19 +1,28 @@
-def predict(data, all_data):
+def predict(data):
 	result = []
 
 	for i in range(0,12):
 		
 		historical_yearly_data = []
 		for year in data:
-			historical_yearly_data.append(int(year[i]))
+			historical_yearly_data.append(int((year[(i)%12]+year[(i+5)%12])/2))
 		historical_avg = sum(historical_yearly_data)/len(historical_yearly_data)
+		result.append(historical_avg)
+		
+	return result
+
+def recent_predict(all_data):
+	
+	result = []
+
+	for i in range(0,12):
 		all_data_avg = 0
 		for j in range(len(all_data)-1, len(all_data)-4, -1):
 			all_data_avg += int(all_data[j])
 		all_data_avg = int(all_data_avg/3)
 		all_data.append(all_data_avg)
 
-		result.append((historical_avg + all_data_avg)/2)
+		result.append(all_data_avg)
 	return result
 
 if __name__ == '__main__':
@@ -24,8 +33,10 @@ if __name__ == '__main__':
 	for i in range(0,years):
 		for j in range(0,12):
 			x = raw_input().split()[1]
-			data[i].append(x)
-			all_data.append(x)
-	predicted_data = predict(data, all_data)
+			data[i].append(float(x))
+			all_data.append(float(x))
+	
+	#predicted_data = predict(data)
+	predicted_data = recent_predict(all_data)
 	for i in predicted_data:
-		print i
+		print int(i)
